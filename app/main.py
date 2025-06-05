a285052a5c88bb0b44504971654cb26a1a11f2f3
Easydxf <amalsaranlaralawrence@gmail.com>
2025-06-05 13:19:58 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -15,4 +15,3 @@ def convert_file(file: UploadFile = File(...)):
 # Only run uvicorn if this script is run directly (not when imported)
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=10000)
-
